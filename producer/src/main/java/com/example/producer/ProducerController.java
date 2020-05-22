9a6f4a6210cef1084b50adbc7d3d8b4322552b57
Shawn Lim <shawn.lim@traveloka.com>
2020-05-22 23:36:18 +0800

    Add push gateway metric

--- producer/src/main/java/com/example/producer/ProducerController.java
+++ producer/src/main/java/com/example/producer/ProducerController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.PushGateway;
+
 @RestController
 public class ProducerController {
   @Autowired
@@ -16,6 +20,30 @@ public class ProducerController {
   @GetMapping("/hello")
   public String hello() {
     return "Hello Producer";
+  }
+
+  @GetMapping("/tryPushGateway")
+  public String tryPushGateway() throws Exception {
+    CollectorRegistry registry = new CollectorRegistry();
+     Gauge duration = Gauge.build()
+         .name("my_batch_job_duration_seconds").help("Duration of my batch job in seconds.").register(registry);
+     Gauge.Timer durationTimer = duration.startTimer();
+     try {
+       // Your code here.
+       Thread.sleep(1000);
+
+       // This is only added to the registry after success,
+       // so that a previous success in the Pushgateway isn't overwritten on failure.
+       Gauge lastSuccess = Gauge.build()
+           .name("my_batch_job_last_success").help("Last time my batch job succeeded, in unixtime.").register(registry);
+       lastSuccess.setToCurrentTime();
+     } finally {
+       durationTimer.setDuration();
+       PushGateway pg = new PushGateway("pushgateway:9091");
+       pg.pushAdd(registry, "my_batch_job");
+     }
+
+     return "PUSHED!";
   }
 
   @PostMapping("/queueJob")
